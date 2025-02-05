d3b47b323eb309aaa73fefb359f1e48f0fa08307
xiqing <qingdiyu1@gmail.com>
2025-02-05 23:56:32 +0800

    完成主页

--- 3d/src/components/Navbar.jsx
+++ 3d/src/components/Navbar.jsx
@@ -74,9 +74,9 @@ const Navbar = () => {
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            } p-6 black-gradient absolute top-9 right-0 mx-4 my-2 !w-full z-10 `}
           >
-            <ul className="list-none flex justify-end items-start flex-1 flex-col gap-4">
+            <ul className="list-none flex  items-center flex-1 flex-col gap-4">
               {navLinks.map((nav) => (
                 <li
                   key={nav.id}
